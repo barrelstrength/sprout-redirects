0ae1b31db581e1450ba142eecb1a5fe976eaf561
Ben Parizek <ben.parizek@gmail.com>
2020-04-21 22:09:38 -0400

    Adds uninstall migration

--- src/migrations/Install.php
+++ src/migrations/Install.php
@@ -7,10 +7,13 @@
 
 namespace barrelstrength\sproutredirects\migrations;
 
+use barrelstrength\sproutbase\base\SproutDependencyInterface;
 use barrelstrength\sproutbase\migrations\Install as SproutBaseInstall;
 use barrelstrength\sproutbaseredirects\migrations\Install as SproutBaseRedirectsInstall;
+use barrelstrength\sproutredirects\SproutRedirects;
 use craft\db\Migration;
 use Throwable;
+use yii\db\Exception;
 
 class Install extends Migration
 {
@@ -35,6 +38,36 @@ class Install extends Migration
         ob_start();
         $migration->safeUp();
         ob_end_clean();
+
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function safeDown(): bool
+    {
+        /** @var SproutRedirects $plugin */
+        $plugin = SproutRedirects::getInstance();
+
+        $sproutBaseRedirectsInUse = $plugin->dependencyInUse(SproutDependencyInterface::SPROUT_BASE_REDIRECTS);
+        $sproutBaseInUse = $plugin->dependencyInUse(SproutDependencyInterface::SPROUT_BASE);
+
+        if (!$sproutBaseRedirectsInUse) {
+            $migration = new SproutBaseRedirectsInstall();
+
+            ob_start();
+            $migration->safeDown();
+            ob_end_clean();
+        }
+
+        if (!$sproutBaseInUse) {
+            $migration = new SproutBaseInstall();
+
+            ob_start();
+            $migration->safeDown();
+            ob_end_clean();
+        }
 
         return true;
     }
