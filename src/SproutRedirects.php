ef2d50b2b73df3fbb6780c1b534ace30530b8eab
Ben Parizek <ben.parizek@gmail.com>
2022-05-03 17:49:11 -0400

    Initial commit, Craft 4

---
+++ src/SproutRedirects.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace BarrelStrength\SproutRedirects;
+
+use BarrelStrength\Sprout\core\db\InstallHelper;
+use BarrelStrength\Sprout\core\db\SproutPluginMigrationInterface;
+use BarrelStrength\Sprout\core\db\SproutPluginMigrator;
+use BarrelStrength\Sprout\core\editions\Edition;
+use BarrelStrength\Sprout\core\modules\Modules;
+use BarrelStrength\Sprout\redirects\RedirectsModule;
+use Craft;
+use craft\base\Plugin;
+use craft\db\MigrationManager;
+use craft\errors\MigrationException;
+use craft\events\RegisterComponentTypesEvent;
+use craft\helpers\UrlHelper;
+use yii\base\Event;
+use yii\base\InvalidConfigException;
+
+class SproutRedirects extends Plugin implements SproutPluginMigrationInterface
+{
+    public const EDITION_LITE = 'lite';
+    public const EDITION_PRO = 'pro';
+
+    public string $minVersionRequired = '1.5.3';
+
+    public string $schemaVersion = '0.0.1';
+
+    /**
+     * @inheritDoc
+     */
+    public static function editions(): array
+    {
+        return [
+            self::EDITION_LITE,
+            self::EDITION_PRO,
+        ];
+    }
+
+    public static function getSchemaDependencies(): array
+    {
+        return [
+            RedirectsModule::class,
+        ];
+    }
+
+    /**
+     * @throws InvalidConfigException
+     */
+    public function getMigrator(): MigrationManager
+    {
+        return SproutPluginMigrator::make($this);
+    }
+
+    public function init(): void
+    {
+        parent::init();
+
+        Event::on(
+            Modules::class,
+            Modules::EVENT_REGISTER_SPROUT_AVAILABLE_MODULES,
+            static function(RegisterComponentTypesEvent $event) {
+                $event->types[] = RedirectsModule::class;
+            }
+        );
+
+        $this->instantiateSproutModules();
+        $this->grantModuleEditions();
+    }
+
+    protected function instantiateSproutModules(): void
+    {
+        RedirectsModule::isEnabled() && RedirectsModule::getInstance();
+    }
+
+    protected function grantModuleEditions(): void
+    {
+        if ($this->edition === self::EDITION_PRO) {
+            RedirectsModule::isEnabled() && RedirectsModule::getInstance()->grantEdition(Edition::PRO);
+        }
+    }
+
+    /**
+     * @throws MigrationException
+     */
+    protected function afterInstall(): void
+    {
+        InstallHelper::runInstallMigrations($this);
+
+        if (Craft::$app->getRequest()->getIsConsoleRequest()) {
+            return;
+        }
+
+        // Redirect to welcome page
+        $url = UrlHelper::cpUrl('sprout/welcome/redirects');
+        Craft::$app->getResponse()->redirect($url)->send();
+    }
+
+    /**
+     * @throws MigrationException
+     * @throws InvalidConfigException
+     */
+    protected function beforeUninstall(): void
+    {
+        InstallHelper::runUninstallMigrations($this);
+    }
+}
