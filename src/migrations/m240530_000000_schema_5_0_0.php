85024899fb906f232523355b42874dc983b5eb1b
Ben Parizek <ben.parizek@gmail.com>
2024-06-29 08:41:39 -0400

    Code cleanup

--- src/migrations/m240530_000000_schema_5_0_0.php
+++ src/migrations/m240530_000000_schema_5_0_0.php
@@ -5,7 +5,6 @@ namespace BarrelStrength\SproutRedirects\migrations;
 use BarrelStrength\Sprout\core\db\m000000_000000_sprout_plugin_migration;
 use BarrelStrength\Sprout\core\db\SproutPluginMigrationInterface;
 use BarrelStrength\SproutRedirects\SproutRedirects;
-use BarrelStrength\SproutSeo\SproutSeo;
 
 class m240530_000000_schema_5_0_0 extends m000000_000000_sprout_plugin_migration
 {
